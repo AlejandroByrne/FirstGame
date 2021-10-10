636a831941b48328811f0985b86c8632251872b1
Alejandro Byrne <alejbyrne@gmail.com>
2021-10-10 17:00:44 -0500

    first commit

---
+++ desktop/src/com/ale/firstgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ale.firstgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ale.firstgame.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
