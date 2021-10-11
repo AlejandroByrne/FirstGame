c9da61fe01baa6859f89d0ee8c858599dd633f2e
Alejandro Byrne <alejbyrne@gmail.com>
2021-10-11 17:43:02 -0500

    second commit

--- desktop/src/com/ale/firstgame/desktop/DesktopLauncher.java
+++ desktop/src/com/ale/firstgame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.ale.firstgame.Launcher;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Water Drop Game";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Launcher(), config);
 	}
 }
